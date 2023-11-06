9b4a81cdec81f8374e34e7d43fab9e8e9cd0a546
Lucy Conner <lucyaconner@gmail.com>
2023-11-06 12:31:58 -0700

    tests

---
+++ FaqMvc/Tests/FakeHttpMessageHandler.cs
@@ -0,0 +1,17 @@
+﻿namespace GptWeb.Tests
+{
+    public class FakeHttpMessageHandler : DelegatingHandler
+    {
+        private readonly HttpResponseMessage _fakeResponse;
+
+        public FakeHttpMessageHandler(HttpResponseMessage responseMessage)
+        {
+            _fakeResponse = responseMessage;
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            return await Task.FromResult(_fakeResponse);
+        }
+    }
+}
