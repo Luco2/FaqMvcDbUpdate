7327a76f8d6f696d2284bed507d8f46bda00c628
Lucy Conner <lucyaconner@gmail.com>
2024-02-07 15:47:27 -0700

    delete vue

--- FaqMvc/obj/Debug/net7.0/GptWeb.AssemblyInfo.cs
+++ FaqMvc/obj/Debug/net7.0/GptWeb.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GptWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a97b6773239eed64bf0743efedbdfe610b1b1c39")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b75e61c485a4f0c68b02c61833799fffa2e92758")]
 [assembly: System.Reflection.AssemblyProductAttribute("GptWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GptWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
